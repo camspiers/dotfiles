220ddbd89162dba5b60575a134422d704d26a421
Cam Spiers <cameron.spiers@heyday.co.nz>
2019-12-28 22:30:32 +1300

    Minor updates

--- files/.config/ubersicht/left.jsx
+++ files/.config/ubersicht/left.jsx
@@ -2,23 +2,20 @@ import getSpacesForDisplay from "./lib/getSpacesForDisplay";
 import { css, run } from 'uebersicht';
 import { defaultTheme } from "./lib/style";
 
-export const initialState = {
-    spaces: [],
-};
+export const initialState = [];
 
 export const updateState = (event, state) => {
   switch(event.type) {
       case 'SPACES_UPDATED':
-        return {
-            ...state,
-            spaces: event.spaces,
-        };
-      default:
-        return state;
+        return event.spaces;
   }
+  return state;
 }
 
-export const command = (dispatch) => getSpacesForDisplay(1).then(spaces => dispatch({ type: 'SPACES_UPDATED', spaces }));
+export const command = (dispatch) => getSpacesForDisplay(1).then(spaces => dispatch({
+    type: 'SPACES_UPDATED',
+    spaces
+}));
 
 export const refreshFrequency = false;
 
@@ -70,16 +67,16 @@ function onClick(dispatch, spaces, space) {
         return;
     }
 
+    run(`yabai -m space --focus ${space.index}`);
+
+    // Optimistic update
     dispatch({
         type: 'SPACES_UPDATED',
         spaces: spaces.map(s => ({...s, focused: space.index === s.index})),
     });
-
-    run(`yabai -m space --focus ${space.index}`);
 }
 
-export const render = ({ spaces }, dispatch) => {
-
+export const render = (spaces, dispatch) => {
     return (
         <div className={spaceContainerClass}>
             {spaces.map(space =>
